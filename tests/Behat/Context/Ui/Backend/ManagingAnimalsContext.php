e62f870381dfb58ec4950373dc9a419221a16d30
Loïc Frémont <lc.fremont@gmail.com>
2020-05-11 14:46:20 +0200

    Init managing animals context

---
+++ tests/Behat/Context/Ui/Backend/ManagingAnimalsContext.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of monofony_demo.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat\Context\Ui\Backend;
+
+use Behat\Behat\Context\Context;
+
+final class ManagingAnimalsContext implements Context
+{
+}
