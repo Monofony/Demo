3244fbec48b6fd3b39cb5c63e6ccd08fd9751f5d
titomtd <magnum35160>
2020-05-18 12:56:26 +0200

    Editing a taxon

--- tests/Behat/Context/Setup/TaxonContext.php
+++ tests/Behat/Context/Setup/TaxonContext.php
@@ -12,9 +12,43 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat\Context\Setup;
 
+use App\Fixture\Factory\TaxonExampleFactory;
 use Behat\Behat\Context\Context;
+use Doctrine\ORM\EntityManagerInterface;
+use Monofony\Bundle\CoreBundle\Tests\Behat\Service\SharedStorageInterface;
 
 final class TaxonContext implements Context
 {
+    /** @var SharedStorageInterface */
+    private $sharedStorage;
+
+    /** @var TaxonExampleFactory */
+    private $taxonFactory;
+
+    /** @var EntityManagerInterface*/
+    protected $manager;
+
+
+    public function __construct(
+        SharedStorageInterface $sharedStorage,
+        TaxonExampleFactory $taxonFactory,
+        EntityManagerInterface $manager
+    ) {
+        $this->sharedStorage = $sharedStorage;
+        $this->taxonFactory = $taxonFactory;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Given there is (also )a taxon with name :name
+     */
+    public function thereIsATaxonWithName(string $name): void
+    {
+        $taxon = $this->taxonFactory->create(['name' => $name]);
+
+        $this->manager->persist($taxon);
+        $this->manager->flush();
+        $this->sharedStorage->set('taxon', $taxon);
+    }
 
 }
---
+++ tests/Behat/Context/Transform/TaxonContext.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Monofony demo.
+ *
+ * (c) Monofony
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace App\Tests\Behat\Context\Transform;
+
+use App\Entity\Taxonomy\Taxon;
+use Behat\Behat\Context\Context;
+use Doctrine\ORM\EntityManagerInterface;
+use Monofony\Bundle\CoreBundle\Tests\Behat\Service\SharedStorageInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
+use Webmozart\Assert\Assert;
+
+final class TaxonContext implements Context
+{
+    /** @var TaxonRepositoryInterface */
+    private $taxonRepository;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(
+        TaxonRepositoryInterface $taxonRepository,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->taxonRepository = $taxonRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Transform :taxon
+     * @Transform /^taxon "([^"]+)"$/
+     */
+    public function getTaxonByName(string $taxonName): Taxon
+    {
+        $taxons = $this->taxonRepository->findByName($taxonName, 'en_US');
+        Assert::eq(
+            count($taxons),
+            1,
+            sprintf('%d taxons has been found with name "%s".', count($taxons), $taxonName)
+        );
+
+        return $taxons[0];
+    }
+
+}
