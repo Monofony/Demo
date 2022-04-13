c768018e4f7231f8b21452955b1052a3232c658e
Loïc Frémont <lc.fremont@gmail.com>
2022-04-13 11:15:17 +0200

    Add pet resource

---
+++ src/Entity/Animal/Pet.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Animal;
+
+use App\Entity\IdentifiableTrait;
+use App\Repository\PetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+#[ORM\Entity(repositoryClass: PetRepository::class)]
+class Pet implements ResourceInterface
+{
+    use IdentifiableTrait;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $name = null;
+
+    #[ORM\Column(type: 'string', unique: true)]
+    #[Gedmo\Slug(fields: ['name'])]
+    private ?string $slug = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    private ?float $size = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $sizeUnit = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $mainColor = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getSize(): ?float
+    {
+        return $this->size;
+    }
+
+    public function setSize(?float $size): void
+    {
+        $this->size = $size;
+    }
+
+    public function getSizeUnit(): ?string
+    {
+        return $this->sizeUnit;
+    }
+
+    public function setSizeUnit(?string $sizeUnit): void
+    {
+        $this->sizeUnit = $sizeUnit;
+    }
+
+    public function getMainColor(): ?string
+    {
+        return $this->mainColor;
+    }
+
+    public function setMainColor(?string $mainColor): void
+    {
+        $this->mainColor = $mainColor;
+    }
+}
---
+++ src/Repository/PetRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Animal\Pet;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\ResourceRepositoryTrait;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+/**
+ * @method Pet|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Pet|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Pet[]    findAll()
+ * @method Pet[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PetRepository extends ServiceEntityRepository implements RepositoryInterface
+{
+    use ResourceRepositoryTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Pet::class);
+    }
+}
