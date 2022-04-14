7c3881632121b3009e4e267b7c25f70841668f9d
Loïc Frémont <lc.fremont@gmail.com>
2022-04-14 16:29:44 +0200

    Add taxon tree

--- src/Entity/Taxonomy/Taxon.php
+++ src/Entity/Taxonomy/Taxon.php
@@ -15,10 +15,34 @@ namespace App\Entity\Taxonomy;
 
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
+use Sylius\Component\Resource\Annotation\SyliusCrudRoutes;
 use Sylius\Component\Taxonomy\Model\Taxon as BaseTaxon;
 
 #[Entity]
 #[Table(name: 'sylius_taxon')]
+#[SyliusCrudRoutes(
+    alias: 'sylius.taxon',
+    path: '/admin/taxa',
+    section: 'backend',
+    redirect: 'update',
+    templates: 'backend/taxon',
+    except: ['show', 'index'],
+    vars: [
+        'all' => [
+            'subheader' => 'sylius.ui.manage_taxons',
+            'templates' => [
+                'form' => 'backend/taxon/_form.html.twig',
+            ],
+        ],
+    ],
+), SyliusRoute(
+    name: 'sylius_admin_partial_taxon_tree',
+    path: '/admin/_partial/taxa/tree',
+    methods: ['GET'],
+    controller: 'sylius.controller.taxon::indexAction',
+    template: '$template',
+    repository: ['method' => 'findRootNodes'],
+)]
 class Taxon extends BaseTaxon implements TaxonInterface
 {
 }
