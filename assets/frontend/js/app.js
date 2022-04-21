4c3ea1334dfa03af2a5ff70fdb824df967cc6767
Loïc Frémont <lc.fremont@gmail.com>
2022-04-21 15:48:00 +0200

    Add frontend layout

--- assets/frontend/js/app.js
+++ assets/frontend/js/app.js
@@ -1,7 +1,9 @@
+import 'foundation-sites';
 import '../scss/main.scss';
 
 import 'babel-polyfill';
-import './shim-semantic-ui';
+// import './shim-semantic-ui';
 
 $(document).ready(function () {
+    $(document).foundation();
 });
