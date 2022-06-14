5c421880072188a936776eef7761130f2d0f53b9
Loïc Frémont <lc.fremont@gmail.com>
2022-06-14 17:19:08 +0200

    Fix coding standard

--- src/Serializer/FileNormalizer.php
+++ src/Serializer/FileNormalizer.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Soliso project.
+ * This file is part of the Monofony demo project.
  *
- * (c) Mobizel
+ * (c) Monofony
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
